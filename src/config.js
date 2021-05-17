9992bb96f6c5ce8156f5c025cbe3f007f332a2a0
priyankachadar <61162836+priyankachadar@users.noreply.github.com>
2021-05-17 14:30:52 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
-    clientId: 'f90c0bc516db44c1a023224bba360193',
-    clientSecret: '33c0f5e6400b4c0a9542d9fcf0819299'
+    clientId: '',
+    clientSecret: ''
   }
 }
